e678311fd6b22cd9b65fce6b65f535bbbc813746
rubayet170746 <rafirubayet75@gmail.com>
2023-11-10 20:35:54 +0600

    added all the fine tuning

--- book_api/serializer.py
+++ book_api/serializer.py
@@ -1,19 +1,41 @@
+# from rest_framework import serializers
+# from book_api.models import Books
+
+# class BookSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     title = serializers.CharField()
+#     number_of_pages = serializers.IntegerField()
+#     published_date = serializers.DateField()
+#     quantity = serializers.IntegerField()
+
+#     def create(self,data):
+#         return Books.objects.create(**data)
+#     def update(self,instance,data):
+#         instance.title = data.get('title',instance.title)
+#         instance.number_of_pages = data.get('number_of_pages',instance.number_of_pages)
+#         instance.published_date = data.get('published_date',instance.published_date)
+#         instance.quantity = data.get('quantity',instance.quantity)
+#         instance.save()
+#         return instance
+
 from rest_framework import serializers
 from book_api.models import Books
+from django.forms import ValidationError
 
-class BookSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField()
-    number_of_pages = serializers.IntegerField()
-    published_date = serializers.DateField()
-    quantity = serializers.IntegerField()
+class BookSerializer(serializers.ModelSerializer):
+    class meta:
+        model = Books
+        fields = '__all__'
+    
+    def validate_title(self,value):
+        if value == 'Diet Coke':
+            raise ValidationError('Diet Coke is not allowed')
+        return value
+    def validate(self,data):
+        if data['quantity'] < 0:
+            raise ValidationError('Quantity cannot be negative')
+        return data
 
-    def create(self,data):
-        return Books.objects.create(**data)
-    def update(self,instance,data):
-        instance.title = data.get('title',instance.title)
-        instance.number_of_pages = data.get('number_of_pages',instance.number_of_pages)
-        instance.published_date = data.get('published_date',instance.published_date)
-        instance.quantity = data.get('quantity',instance.quantity)
-        instance.save()
-        return instance
+    
+
+
--- book_api/urls.py
+++ book_api/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from book_api.views import book_list,book_create,book
+from book_api.views import BookList,BookCreate,BookDetail
+
 
 urlpatterns = [
     path('',book_create),
+    path('',BookCreate.as_view()),
     path('list/',book_list),
     path('<int:pk>',book),
+     path('<int:pk>',BookDetail.as_view()),
+    path('list/',BookList.as_view()),
 ]
